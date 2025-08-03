7ed851ed87d59c21751001ad4dff82119c032fb1
Ubuntu <ubuntu@ip-172-31-10-109.us-west-1.compute.internal>
2025-08-03 15:54:15 +0000

    push name changes

--- infra/remote_backend_s3.tf
+++ infra/remote_backend_s3.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "dev-proj-1-remote-state-bucket-nikhil-123456"
-    key    = "python-application-infra/terraform.tfstate"
+    key    = "terraform.tfstate"
     region = "us-east-1"
   }
 }
