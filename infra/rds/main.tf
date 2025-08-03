ae5abf3d698a7beeea4b66a6bfa604748015a4ee
Ubuntu <ubuntu@ip-172-31-10-109.us-west-1.compute.internal>
2025-08-03 16:40:53 +0000

    Jenkinsfile changes

--- infra/rds/main.tf
+++ infra/rds/main.tf
@@ -13,11 +13,11 @@ resource "aws_db_subnet_group" "dev_proje_1_db_subnet_group" {
 }
 
 resource "aws_db_instance" "default" {
-  allocated_storage       = 10
+  allocated_storage       = 20
   storage_type            = "gp2"
   engine                  = "mysql"
-  engine_version          = "5.7"
-  instance_class          = "db.t2.micro"
+  engine_version          = "8.0.35"
+  instance_class          = "db.t3.micro"
   identifier              = var.mysql_db_identifier
   username                = var.mysql_username
   password                = var.mysql_password
