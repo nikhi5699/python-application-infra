8d91dd1637d7a20d09fc3be1aef3a6aa5485847b
Ubuntu <ubuntu@ip-172-31-10-109.us-west-1.compute.internal>
2025-08-04 03:41:09 +0000

    main.tf changes

--- infra/main.tf
+++ infra/main.tf
@@ -49,7 +49,7 @@ module "alb" {
   lb_name                   = "dev-proj-1-alb"
   is_external               = false
   lb_type                   = "application"
-  sg_for_jenkins            = [module.security_group.sg_ec2_sg_ssh_http_id, module.security_group.sg_ec2_for_python_api]
+  sg_enable_ssh_https       = module.security_group.sg_ec2_sg_ssh_http_id
   subnet_ids                = tolist(module.networking.dev_proj_1_public_subnets)
   tag_name                  = "dev-proj-1-alb"
   lb_target_group_arn       = module.lb_target_group.dev_proj_1_lb_target_group_arn
